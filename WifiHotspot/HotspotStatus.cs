72d78e522c862ab01ce938b2b9474b1804f50aba
David Dostal <david.dostal1112@gmail.com>
2018-10-05 12:43:16 +0200

    Move enum utilities into separate file

--- WifiHotspot/HotspotStatus.cs
+++ WifiHotspot/HotspotStatus.cs
@@ -22,22 +22,4 @@ namespace WifiHotspot
         [Description("Unknown")]
         Unknown,
     }
-
-    public static class EnumExtensions
-    {
-        public static string GetDescription<T>(this T value) where T : struct
-        {
-            if (!value.GetType().IsEnum)
-                throw new ArgumentException("Value must be of type enum.");
-
-            MemberInfo[] memberInfo = value.GetType().GetMember(value.ToString());
-            if (memberInfo != null && memberInfo.Length > 0)
-            {
-                object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
-                if (attrs != null && attrs.Length > 0)
-                    return ((DescriptionAttribute)attrs[0]).Description;
-            }
-            return value.ToString();
-        }
-    }
 }
---
+++ WifiShare/Utils/EnumUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+public static class Utils
+{
+    public static string GetDescription<T>(this T value) where T : struct
+    {
+        if (!value.GetType().IsEnum)
+            throw new ArgumentException("Value must be of type enum.");
+
+        MemberInfo[] memberInfo = value.GetType().GetMember(value.ToString());
+        if (memberInfo != null && memberInfo.Length > 0)
+        {
+            object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attrs != null && attrs.Length > 0)
+                return ((DescriptionAttribute)attrs[0]).Description;
+        }
+        return value.ToString();
+    }
+}
